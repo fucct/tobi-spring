400f37f8035252cd55ba5d40278f75ba82c85caa
dd <dqrd123@gmail.com>
2020-09-21 10:50:18 +0900

    3.6 jdbcTemplate

--- src/main/java/com/fucct/tobispring/dao/UserDao.java
+++ src/main/java/com/fucct/tobispring/dao/UserDao.java
@@ -1,126 +1,38 @@
 package com.fucct.tobispring.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Objects;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.fucct.tobispring.user.User;
 
 public class UserDao {
-    private DataSource dataSource;
-    private JdbcContext context;
+    private final JdbcTemplate jdbcTemplate;
+    private RowMapper<User> userRowMapper;
 
     public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.context = new JdbcContext(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy st = connection -> {
-            PreparedStatement ps = connection.prepareStatement(
-                "insert into accounts(id, name, password) values(?,?,?)");
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-
-            return ps;
-        };
-        context.workWithStatementStrategy(st);
+        this.jdbcTemplate.update("insert into accounts(id, name, password) values(?,?,?)", user.getId(), user.getName(),
+            user.getPassword());
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select * from accounts where id = ?");
-            ps.setString(1, id);
-            rs = ps.executeQuery();
-            User user = null;
-            if (rs.next()) {
-                user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-            }
-            if (Objects.isNull(user)) {
-                throw new EmptyResultDataAccessException(1);
-            }
-
-            return user;
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
-
-
+        return this.jdbcTemplate.queryForObject("select * from accounts where accounts.id = ?",
+            new Object[] {id},
+            userRowMapper);
+     }
 
     public void deleteAll() throws SQLException {
-        context.executeSql("delete from accounts");
+        this.jdbcTemplate.update(connection -> connection.prepareStatement("delete from accounts"));
     }
 
-    public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from accounts");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
-
-    public void setContext(final DataSource dataSource) {
-        this.context.setDataSource(dataSource);
+    public Integer getCount() throws SQLException {
+        return this.jdbcTemplate.queryForObject("select count(*) from accounts", Integer.class);
     }
 }
